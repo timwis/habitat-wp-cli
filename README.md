a8e2b883daaa3e50f8c03acb9c39208615113d9c
timwis <tim@timwis.com>
2018-07-06 07:02:50 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Habitat package: wp-cli
+
+A [habitat](https://habitat.sh) package for the [WordPress CLI](https://wp-cli.org/).
---
+++ plan.sh
@@ -0,0 +1,33 @@
+pkg_name=wp-cli
+pkg_origin=timwis
+pkg_version="1.5.1"
+pkg_maintainer="timwis <tim@timwis.com>"
+pkg_description="The command line interface for WordPress"
+pkg_upstream_url="https://wp-cli.org/"
+pkg_license=("MIT")
+pkg_source="https://github.com/wp-cli/wp-cli/releases/download/v${pkg_version}/wp-cli-${pkg_version}.phar"
+pkg_filename="wp-cli.phar"
+pkg_shasum="0cc7a95e68a2ef02fc423614806c29a8e76e4ac8c9b3e67d6673635d6eaea871"
+pkg_deps=(core/php)
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_check() {
+  "$(pkg_path_for core/php)"/bin/php "../${pkg_filename}" --info 2>&1 | grep -q ${pkg_version}
+}
+
+do_install() {
+  install --verbose -D --mode +x "../${pkg_filename}" "${pkg_prefix}/bin/${pkg_filename}"
+
+  cat<<EOF > "${pkg_prefix}/bin/wp-cli"
+#!/bin/sh
+"$(pkg_path_for core/php)"/bin/php "${pkg_prefix}/bin/${pkg_filename}" "\$@"
+EOF
+  chmod +x "${pkg_prefix}/bin/wp-cli"
+}
